59e4895e3db11d9dc35123c82dc7a83bc5d83f97
Фёдор Алексеев <f.alexeeff2010@yandex.ru>
2021-09-28 18:28:25 +0300

    Добавьте файлы проекта.

---
+++ engine/Engine.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace engine
+{
+    static public class Engine
+    {
+    }
+}
---
+++ heroes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace heroes
+{
+    class Program
+    {
+        public const string cpp = @".\engine.dll";
+
+        [DllImport(cpp, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int CSEngineInit();
+
+        [DllImport(cpp, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int CSEngineQuit();
+
+        [DllImport(cpp, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool CSEventCheck();
+
+        static void Main(string[] args)
+        {
+            CSEngineInit();
+
+            while (CSEventCheck())
+            {
+
+            }
+
+            CSEngineQuit();
+        }
+    }
+}
