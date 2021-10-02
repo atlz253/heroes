edcacbb63b44f73929e1d280c4bcf862f092832e
Fedor <f.alexeeff2010@yandex.ru>
2021-10-03 00:15:07 +0300

    random grass background

--- heroes/Program.cs
+++ heroes/Program.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 
 namespace heroes
 {
-    class Program
+    static class Program
     {
         private const int screenSize = 960, cellSize = 32;
-
+        private static Random rnd = new Random();
         private static List<Line> vertical = new List<Line>(), horizontal = new List<Line>();
+        private static Entity[,] background = new Entity[screenSize/cellSize, screenSize/cellSize];
 
         private static void InitGrid()
         {
@@ -19,6 +20,13 @@ namespace heroes
             }
         }
 
+        private static void InitBackground()
+        {
+            for (int i = 0; i < screenSize / cellSize; i++)
+                for (int j = 0; j < screenSize / cellSize; j++)
+                    background[i, j] = new Entity((cellSize, cellSize, (short) (i * cellSize), (short) (j * cellSize)), "/home/fedor/Desktop/heroes/heroes/heroes/res/textures.png", (cellSize, cellSize, (short)(cellSize * rnd.Next(2)), (short)(cellSize * rnd.Next(2))));
+        }
+
         private static void RenderGrid()
         {
             foreach (Line line in vertical)
@@ -27,16 +35,25 @@ namespace heroes
                     line.Render();
         }
 
+        private static void RenderBackground()
+        {
+            for (int i = 0; i < screenSize / cellSize; i++)
+                for (int j = 0; j < screenSize / cellSize; j++)
+                    background[i, j].Render();
+        }
+
         static void Main(string[] args)
         {
             Engine.Init(screenSize, screenSize);
 
             InitGrid();
+            InitBackground();
 
             while (Event.Check())
             {
                 Render.Clear();
-
+                
+                RenderBackground();
                 RenderGrid();
 
                 Render.Present();
