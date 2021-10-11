de38657bbf2989c8aa3dcb852e313586cee32a11
Fedor <f.alexeeff2010@yandex.ru>
2021-10-11 12:56:07 +0300

    player class

---
+++ heroes/Player.cs
@@ -0,0 +1,14 @@
+using engn;
+
+namespace heroes
+{
+    public class Player
+    {
+        public bool Turn { get; set; }
+
+        public Player() // TODO: string name
+        {
+            Turn = true;
+        }
+    }
+}
